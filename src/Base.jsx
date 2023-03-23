184d1c39a7233248075986468a10b51959f75c5f
Amirkhan Salan <68302923+n7wt@users.noreply.github.com>
2023-03-23 18:13:10 +0300

    Third task

---
+++ src/Base.jsx
@@ -0,0 +1,13 @@
+function Base() {
+  return (
+    <>
+      <h1>Приветствую!</h1>
+      <img
+        src="https://cdn-images-1.medium.com/max/2000/1*qXcjSfRj0C0ir2yMsYiRyw.jpeg"
+        alt="react"
+      />
+    </>
+  );
+}
+
+export default Base;
